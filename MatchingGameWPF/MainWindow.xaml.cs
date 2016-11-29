e9314a569f1fd53a5e8ea0ea8876d0dfd0af06a7
Nick <kedomir@gmail.com>
2016-11-29 04:44:22 +0000

    Working on Tutorial 3, currently working on Step 3.
    Assigning random symbols to the squares in the grid doesn't work as coded in the walkthrough, because the WPF objects differ from the WinForms versions the tutorial is built on.

---
+++ MatchingGameWPF/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingGameWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected Random rand;
+        protected List<string> symbols;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            rand = new Random();
+            symbols = new List<string>()
+            {
+                "!", "!", "N", "N", ",", ",", "k", "k",
+                "b", "b", "v", "v", "w", "w", "z", "z"
+            };
+        }
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            /*
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in matchGrid.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+            */
+        }
+    }
+}
